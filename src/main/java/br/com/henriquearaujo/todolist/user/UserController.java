3203706ea443f2d42762b0c0dbb5f75e107c9f5f
Henrique-Arau <penielh43@gmail.com>
2023-10-10 13:08:17 -0300

    Integração com o Banco De Dados

--- src/main/java/br/com/henriquearaujo/todolist/user/UserController.java
+++ src/main/java/br/com/henriquearaujo/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.henriquearaujo.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +29,23 @@ public class UserController {
      * Date (data)
      * void
      */
+     
+     @Autowired
+     private IUserRepository userRepository;
 
      @PostMapping("/")
-     public void create(@RequestBody UserModel userModel) {
-         System.out.println(userModel.name);
+     public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        
+        if(user != null){
+            System.out.println("Usuário já existe");
+            //Mensagem de erro
+            //status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
      }
 
 
